5c1f1d056c600af7568ab36cc31a587f9ec3b9d0
Rick Elton <richelton14@gmail.com>
2018-11-04 15:58:06 -0300

    Listagens de produtos consumidos da API

--- ReactJS/huntweb/src/pages/main/index.js
+++ ReactJS/huntweb/src/pages/main/index.js
@@ -3,17 +3,32 @@ import React, { Component } from "react";
 import api from "../../services/api";
 
 export default class Main extends Component {
+    
+    // Variaveis de estado.
+    // Quando uma das variaveis mudam, ele manda executar o render novamente.
+    state = {
+        products: [],
+    };
+    
     // Método executa assim que o componente é exibido na tela.
     componentDidMount() {
         this.loadProducts();
-    }
+    };
 
     loadProducts = async () => {
         const response = await api.get("/products");
-        console.log(response.data.docs);
+        
+        // Modificando variaveis de estado.
+        this.setState({ products: response.data.docs });
     };
 
     render() {
-        return <h1>Hello!</h1>
+        return (
+            <div className="products-list">
+                { this.state.products.map(product => (
+                    <h2 key={ product._id } >{ product.title }</h2>
+                )) }
+            </div>
+        );
     }
 }
