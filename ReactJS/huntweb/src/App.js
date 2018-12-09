2173f7520bb8a815b33d034fd1f62e39ee3f99da
Rick Elton <richelton14@gmail.com>
2018-12-08 21:44:18 -0300

    Corrigindo erro no component Header

--- ReactJS/huntweb/src/App.js
+++ ReactJS/huntweb/src/App.js
@@ -1,15 +1,16 @@
 import React from 'react';
 
 import "./styles.css";
-import Header from "./components/Header";
+import Header from "./components/Header/index";
 import Routes from "./routes";
 
-
-const App = () => (
-	<div className="App">
-    	<Header />
-		<Routes />
-	</div>
-);
-
-export default App;
+export default class App extends React.Component {
+	render() {
+		return (
+			<div>
+    			<Header />
+				<Routes />
+			</div>
+		)
+	}
+}
--- ReactJS/huntweb/src/components/Header/index.js
+++ ReactJS/huntweb/src/components/Header/index.js
@@ -5,12 +5,5 @@ import "./styles.css";
 const Header = () => (
     <header id="main-header">JSHunt</header>
 );
-// Mesma coisa de:
-/*class Header extends Component {
-    render() {
-        return <h1>Hello!</h1>
-    }
-}
-*/
 
 export default Header;
