d56bccdabfbc581646011dce3f130c32ed464110
Rich Elton <richelton14@gmail.com>
2018-12-11 12:51:14 -0300

    Começando aplicaomeçando aplicação web da camara dos deputados

---
+++ deputados/src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+//import Deputado from './Components/Deputados/deputado';
+
+class App extends Component {
+
+  render() {
+    return (
+      <div>
+        <p>ooo</p>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ deputados/src/Components/Deputados/deputado.js
@@ -0,0 +1,7 @@
+import React, { Component } from 'react';
+
+export default class Deputado extends Component {
+
+    
+
+}
