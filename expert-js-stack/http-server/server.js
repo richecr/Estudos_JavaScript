62ca28db89e30e2ce0d69356e463c67e6f2a3f2c
Rick Elton <richelton14@gmail.com>
2019-01-21 11:07:06 -0300

    Organizando rotas: Criação de rotas GET e também POST

--- expert-js-stack/http-server/server.js
+++ expert-js-stack/http-server/server.js
@@ -4,53 +4,64 @@ const app = express()
 const path = require('path')
 
 app.get("/", (req, res)=>{
-  res.sendFile(path.join(__dirname + "/index.html"));
+	res.sendFile(path.join(__dirname + "/index.html"));
 })
 
 var adminRouter = express.Router();
 
-// middleware de roteamento exeutado a cada requisição
-adminRouter.use((req, res, next) => {
-  
-  // logar cada requisição no console
-  console.log(req.method, req.url)
-  
-  // continue com o que precisar ser feito e vá para a rota
-  next()
+	// middleware de roteamento exeutado a cada requisição
+	adminRouter.use((req, res, next) => {
+
+	// logar cada requisição no console
+	console.log(req.method, req.url)
+
+	// continue com o que precisar ser feito e vá para a rota
+	next();
 })
 
 // Cria rota para página principal. (http://localhost:8000/admin)
 adminRouter.get("/", function(req, res){
-  res.send("Eu sou o dashboard!");
+	res.send("Eu sou o dashboard!");
 })
 
 // Cria rota para página de usuários. (http://localhost:8000/admin/users)
 adminRouter.get("/users", function(req, res){
-  res.send("Aqui listamos todos os users!");
+	res.send("Aqui listamos todos os users!");
 })
 
 // Cria rota para página de posts. (http://localhost:8000/admin/posts)
 adminRouter.get("/posts", function(req, res) {
-  res.send("Aqui listamos todos os posts!")
+	res.send("Aqui listamos todos os posts!")
 })
 
-// Midleware para validação de paramentros, nessa caso para o 'name'.
+// Middleware para validação de paramentros, nessa caso para o 'name'.
 adminRouter.param("name", (req, res, next, name)=>{
-  
-  // Aqui seria realizado a validação.
-  console.log("Validando nome: " + name);
-  
-  // quando a validação acabar, salve o novo nome na requisição
-  req.name = name;
-  
-  // Vai para próxima coisa a ser feita.
-  next();
+
+	// Aqui seria realizado a validação.
+	console.log("Validando nome: " + name);
+
+	// quando a validação acabar, salve o novo nome na requisição
+	req.name = name;
+
+	// Vai para próxima coisa a ser feita.
+	next();
 })
 
 // rota com parâmetros. (http://localhost:8000/admin/users/:name)
 adminRouter.get("/users/:name", (req, res)=>{
-  res.send("Fala DEV " + req.name + "!");
+	res.send("Fala DEV " + req.name + "!");
 })
+
+// Rotas para login, a de acesso e a que o cliente envia para o servidor.
+// Poderia ser definido diretamente no 'adminRouter'.
+app.route("/login")
+	.get((req, res)=>{
+		res.send("Este é o formulário de login.");
+	})
+
+	.post((req, res)=>{
+		res.send("Processando o formulário de login...");
+	})
 
 // Adiciona essas rotas a aplicação principal.
 app.use("/admin", adminRouter);
