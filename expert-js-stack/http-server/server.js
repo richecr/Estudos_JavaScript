408af289bb0455c1e29b799510da98f90b64df93
Rick Elton <richelton14@gmail.com>
2019-01-21 00:58:07 -0300

    Aplicação utilizando express: enviando uma página HTML para o cliente

--- expert-js-stack/http-server/server.js
+++ expert-js-stack/http-server/server.js
@@ -1,18 +1,13 @@
-// vamos importar os módulos http e filesystem
-const http = require('http'), fs = require('fs')
+// vamos começar, carregando o express e criando a app
+const app = require('express')(),
+      path = require('path')
 
-// então, criamos o servidor http
-http.createServer((req, res) => {
-  // esse é o cabeçalho da nossa resposta
-  res.writeHead(200, {
-    'Content-Type': 'text/html',
-    'Access-Control-Allow-Origin' : '*'
-  })
-  // leremos o index.html
-  let readStream = fs.createReadStream(__dirname + '/index.html')
-  // então, enviamos ele para o cliente
-  readStream.pipe(res)
-}).listen(8000)
+// então, criamos uma rota para '/'
+app.get('/', (req, res) => {
+  // aqui precisamos enviar o index.html para o cliente
+  res.sendFile(path.join(__dirname + '/index.html'))
+})
 
-// no fim, informamos o endpoint para o usuário
-console.log('Visite-me em: http://localhost:8000')
+// no fim, iniciamos o serviço na porta 8001
+app.listen(8001)
+console.log('8001 é a porta mágica!')
