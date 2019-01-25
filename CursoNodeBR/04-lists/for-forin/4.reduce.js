2ae6563dddb57f6adb7ef6be31b7d4330b7b56ed
Rickecr <richelton14@gmail.com>
2019-01-25 17:30:05 -0300

    Criando meu Array.meuReduce()

--- CursoNodeBR/04-lists/for-forin/4.reduce.js
+++ CursoNodeBR/04-lists/for-forin/4.reduce.js
@@ -1,16 +1,38 @@
 const { obterPessoas } = require("./service");
 
+Array.prototype.meuReduce = function (callback, valorInicial) {
+    let valorFinal = typeof valorInicial !== undefined ? valorInicial : this[0];
+    for (let index = 0; index <= this.length - 1; index++) {
+        valorFinal = callback(valorFinal, this[index], this);
+    }
+    return valorFinal;
+}
+
 async function main() {
     try {
         const { results } = await obterPessoas("a");
 
         const pesos = results.map((pessoa) => parseInt(pessoa.height))
 
-        const total = pesos.reduce((anterior, proximo) => {
+        /*const total = pesos.reduce((anterior, proximo) => {
             return anterior + proximo;
-        })
+        })*/
+
+        const minhaLista = [
+            ['Rick', "Elton"],
+            ["Pedro", "Rosiene"]
+        ]
+        const total = minhaLista.meuReduce((anterior, proximo) => {
+            return anterior.concat(proximo);
+        }, []).join(',');
 
         console.log("total: ", total);
+
+        const totalSoma = pesos.meuReduce((anterior, proximo) => {
+            return anterior + proximo;
+        }, 0)
+
+        console.log("Soma total: ", totalSoma);
 
     } catch (error) {
         console.error("Ihh rapaz", error);
