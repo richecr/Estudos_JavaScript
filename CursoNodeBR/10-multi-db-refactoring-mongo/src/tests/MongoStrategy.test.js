e8fcde3e157350ab35d3e06780b7abecb04f74b1
Rickecr <richelton14@gmail.com>
2019-02-11 13:47:25 -0300

    Refatorando código: Estrategia do MongoDB

---
+++ CursoNodeBR/10-multi-db-refactoring-mongo/src/tests/MongoStrategy.test.js
@@ -0,0 +1,58 @@
+const assert = require('assert');
+const Mongo = require('../db/strategies/mongodb/Mongo');
+const Context = require('../db/strategies/base/ContextStrategy');
+const HeroisSchema = require('../db/strategies/mongodb/Schemas/HeroisSchema');
+
+let context = {};
+
+const MOCK_CADASTRAR = {
+    nome: 'Batman',
+    poder: 'Dinheiro'
+};
+
+const MOCK_ATUALIZAR = {
+    nome: 'Flash',
+    poder: 'Rápido'
+};
+
+const MOCK_DELETAR = {
+    nome: 'Homem de Ferro',
+    poder: 'Lata'
+}
+
+describe.only("Mongo strategy", function () {
+    this.beforeAll(async () => {
+        const connection = Mongo.connect();
+        context = new Mongo(connection, HeroisSchema);
+        await context.create(MOCK_ATUALIZAR);
+        await context.create(MOCK_DELETAR);
+    })
+    
+    it('Verificar conexão', async () => {
+        const result = await context.isConnected();
+        const expect = "Conectado";
+        assert.deepEqual(result, expect);
+    });
+
+    it('Cadastrar um heroi', async () => {
+        const { nome, poder } = await context.create( MOCK_CADASTRAR );
+        
+        assert.deepEqual({ nome, poder }, MOCK_CADASTRAR);
+    });
+
+    it('Listar herois', async () => {
+        const { nome, poder } = await context.read({ nome: 'Batman' });
+        assert.deepEqual({nome, poder}, MOCK_CADASTRAR);
+    });
+
+    it('Atualizar um heroi', async () => {
+        await context.update({ nome: 'Flash'}, { poder: "Velocidade" });
+        const { nome, poder } = await context.read({ nome: "Flash" });
+        assert.deepEqual({ nome, poder }, { nome: "Flash", poder: "Velocidade"});
+    });
+
+    it('Deletando um heroi', async () => {
+        const result = await context.delete({nome: 'Homem de Ferro'});
+        assert.deepEqual(result.n, 1);
+    });
+})
---
+++ CursoNodeBR/10-multi-db-refactoring-mongo/src/tests/PostgresStrategy.test.js
@@ -0,0 +1,60 @@
+const assert = require('assert');
+const Postgres = require('../db/strategies/Postgres');
+const Context = require('../db/strategies/base/ContextStrategy');
+
+const context = new Context(new Postgres());
+const MOCK_HEROI_CADASTRAR = {
+    nome: 'Gaviao Negro',
+    poder: 'flexas'
+};
+
+const MOCK_HEROI_ATUALIZAR = {
+    nome: 'Homem-Aranha',
+    poder: 'Ser nerd'
+};
+
+describe('postgres strategy', function() {
+    this.timeout(Infinity);
+
+    this.beforeAll(async function () {
+        await context.connect();
+        //await context.delete();
+        await context.create(MOCK_HEROI_ATUALIZAR);
+    });
+
+    it('Postgres Connection', async function() {
+        const result = await context.isConnected();
+        assert.equal(result, true);
+    });
+
+    it('Cadastrar um heroi', async function() {
+        const result = await context.create( MOCK_HEROI_CADASTRAR );
+        delete result.id;
+        assert.deepEqual(result, MOCK_HEROI_CADASTRAR);
+    });
+
+    it('Buscar por um heroi', async function () {
+        const [result] = await context.read({ nome: MOCK_HEROI_CADASTRAR.nome });
+        delete result.id;
+        assert.deepEqual(result, MOCK_HEROI_CADASTRAR);
+    });
+
+    it('Atualizar um heroi', async function () {
+        const [itemAtualizar] = await context.read({ nome: MOCK_HEROI_ATUALIZAR.nome });
+        const novoItem = {
+            ...MOCK_HEROI_ATUALIZAR,
+            nome: 'Mulher-Maravilha'
+        };
+        const [result] = await context.update(itemAtualizar.id, novoItem);
+        const [itemAtualizado] = await context.read({ id: itemAtualizar.id });
+        assert.deepEqual(itemAtualizado.nome, novoItem.nome);
+        assert.deepEqual(result, 1);
+    });
+
+    it('Deletar um heroi', async function () {
+        const [item] = await context.read({});
+
+        const result = await context.delete(item.id);
+        assert.deepEqual(result, 1);
+    })
+})
